30802fc480c5f0ccf60ff68c767cd399e774bd67
Anders Steiner <anders.steiner@gmail.com>
2015-09-19 10:15:40 +0200

    Renamed tests

--- src/test/java/com/github/webdriverextensions/InstallDriversMojoTest.java
+++ src/test/java/com/github/webdriverextensions/InstallDriversMojoTest.java
@@ -7,7 +7,7 @@ import java.io.File;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class InstallDriversMojoTest extends AbstractInstallDriverMojoTest {
-    
+
     public void test_raise_error_when_driver_was_not_found_in_repository() throws Exception {
         InstallDriversMojo mojo = getMojo("src/test/resources/driver_not_found_in_repositoy_pom.xml", "install-drivers");
         mojo.repositoryUrl = Thread.currentThread().getContextClassLoader().getResource("repository.json");
@@ -20,7 +20,7 @@ public class InstallDriversMojoTest extends AbstractInstallDriverMojoTest {
         }
     }
 
-    public void test_configuration_extract_phantom_j_s_driver_from_tar_bz_() throws Exception {
+    public void test_configuration_extract_phantom_j_s_driver_from_tar_bz2() throws Exception {
         InstallDriversMojo mojo = getMojo("src/test/resources/phantomjs_extract_pom.xml", "install-drivers");
         mojo.repositoryUrl = Thread.currentThread().getContextClassLoader().getResource("repository.json");
 
@@ -30,7 +30,7 @@ public class InstallDriversMojoTest extends AbstractInstallDriverMojoTest {
         assertNumberOfInstalledDriverIs(1);
     }
 
-    public void test_configuration_with_custon_driver_not_in_repository() throws Exception {
+    public void test_configuration_with_custom_driver_not_in_repository() throws Exception {
         // Given
         InstallDriversMojo mojo = getMojo("src/test/resources/custom_driver_pom.xml", "install-drivers");
         mojo.repositoryUrl = Thread.currentThread().getContextClassLoader().getResource("repository.json");
