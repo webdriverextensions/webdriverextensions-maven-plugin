d35e770fef562612d36b747bad0d525977ec62d6
Anders Steiner <anders.steiner@gmail.com>
2016-05-20 06:54:05 +0200

    Pollished log output

--- src/main/java/com/github/webdriverextensions/DriverDownloader.java
+++ src/main/java/com/github/webdriverextensions/DriverDownloader.java
@@ -79,9 +79,9 @@ public class DriverDownloader {
         try {
             List<String> fileContent = Files.readAllLines(Paths.get("/Users/anders/Temp/cache/old/phantomjs-linux-64bit-1.9.8/phantomjs-1.9.8-linux-x86_64.tar.bz2"), StandardCharsets.UTF_8);
             if (fileContent.get(0).startsWith("<?xml")) {
-                mojo.getLog().info("Downloaded driver file contains the following error message");
+                mojo.getLog().info("  Downloaded driver file contains the following error message");
                 for (String line : fileContent) {
-                    mojo.getLog().info(line);
+                    mojo.getLog().info("  " + line);
                 }
             }
         } catch (Exception e) {
