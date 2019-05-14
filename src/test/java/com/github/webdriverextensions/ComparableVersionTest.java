932d11897c52e1c22e3c342831cbe2d571383062
Anders Steiner <anders.steiner@gmail.com>
2019-05-14 20:32:43 +0200

    Added ComparableVersionTest

---
+++ src/test/java/com/github/webdriverextensions/ComparableVersionTest.java
@@ -0,0 +1,24 @@
+package com.github.webdriverextensions;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ComparableVersionTest {
+
+    @Test
+    public void testConstructor() {
+        assertThat(new ComparableVersion("2.45.0").compareTo(new ComparableVersion("2.46.0")), is(-1));
+        assertThat(new ComparableVersion("2.46.0").compareTo(new ComparableVersion("2.46.0")), is(0));
+        assertThat(new ComparableVersion("2.46.0").compareTo(new ComparableVersion("2.45.0")), is(1));
+
+        assertThat(new ComparableVersion("2.46.0").compareTo(new ComparableVersion("70.0.3538.16")), is(-1));
+        assertThat(new ComparableVersion("70.0.3538.16").compareTo(new ComparableVersion("70.0.3538.16")), is(0));
+        assertThat(new ComparableVersion("70.0.3538.16").compareTo(new ComparableVersion("2.46.0")), is(1));
+
+        assertThat(new ComparableVersion("70.0.3538.16").compareTo(new ComparableVersion("70.0.3538.67")), is(-1));
+        assertThat(new ComparableVersion("70.0.3538.67").compareTo(new ComparableVersion("70.0.3538.67")), is(0));
+        assertThat(new ComparableVersion("70.0.3538.67").compareTo(new ComparableVersion("70.0.3538.16")), is(1));
+    }
+}
