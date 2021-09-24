241d40463111443be842257fd22615a474d919a1
Ronny Perinke <sephiroth@sephiroth-j.de>
2021-09-24 21:31:33 +0200

    replace deprecated org.junit.Assert.assertThat

--- src/test/java/com/github/webdriverextensions/ComparableVersionTest.java
+++ src/test/java/com/github/webdriverextensions/ComparableVersionTest.java
@@ -2,8 +2,8 @@ package com.github.webdriverextensions;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class ComparableVersionTest {
 
--- src/test/java/com/github/webdriverextensions/RepositoryTest.java
+++ src/test/java/com/github/webdriverextensions/RepositoryTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class RepositoryTest {
 
